99a320f966aa57d48797a9f583786171dbc70f9d
tahmidman20 <tahmid7282@gmail.com>
2025-09-07 19:40:43 +0500

    calling api and card show on display

--- script.js
+++ script.js
@@ -1,20 +1,72 @@
 const categoryContainer = document.getElementById("categoryContainer");
 
+const newsContainer = document.getElementById("newsContainer");
+
 const loadCategory = () => {
   fetch("https://openapi.programming-hero.com/api/categories")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.categories);
       const categories = data.categories;
-      categories.forEach((catG) => {
-        categoryContainer.innerHTML += ` 
-        <li class="hover:bg-[#15803d]">${catG.category_name}</li>
-        
-        `;
-      });
+      //   console.log(categories);
+      showCategory(categories);
     })
     .catch((err) => {
       console.log(err);
     });
 };
+// 20=+
+const showCategory = (categories) => {
+  categories.forEach((catG) => {
+    categoryContainer.innerHTML += ` 
+        <li id="${catG.id}" class="hover:bg-[#15803d]
+         hover:text-white p-1 font-semibold">${catG.category_name}</li>
+        
+        `;
+  });
+  categoryContainer.addEventListener("click", (e) => {
+    const allLi = document.querySelectorAll("li");
+    allLi.forEach((li) => {
+      li.classList.remove("bg-[#15803d]", "text-white", "rounded-sm");
+      li.addEventListener("click", () => {
+        console.log(li);
+      });
+    });
+
+    if (e.target.localName === "li") {
+      console.log(e.target.id);
+      // console.log(e.target.localName);
+      e.target.classList.add("bg-[#15803d]", "text-white", "rounded-sm");
+      loadNewsByCategory(e.target.id);
+    }
+  });
+};
+
+const loadNewsByCategory = (categoryId) => {
+  console.log(categoryId);
+  fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data.plants);
+
+      showNewsByCategory(data.plants);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const showNewsByCategory = (plants) => {
+  console.log(plants);
+  plants.forEach((plant) => {
+    newsContainer.innerHTML += `
+    <div>
+    <img src="${plant.image}" >
+        <h1>
+        ${plant.name}
+         </h1>
+    </div>
+    `;
+  });
+};
+
 loadCategory();
